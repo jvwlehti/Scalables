08d22f12af402ce0400ac9d46e52b20370d932c8
Jere Lehtinen <93128297+jvwlehti@users.noreply.github.com>
2024-12-13 18:04:26 +0200

    Add coursework 5.2

---
+++ 5.2 Items-API-and-multiple-servers/todo-api/app.js
@@ -0,0 +1,86 @@
+import { postgres } from './deps.js'
+
+const sql = postgres({});
+
+const handleGetRoot = async () => {
+    return new Response("Hello from the another side");
+};
+
+const getTodo = async (request, urlPatternResult) => {
+
+    const id = urlPatternResult.pathname.groups.id;
+    const todos = await sql`SELECT * FROM todos WHERE id = ${ id }`;
+    if (todos.length === 0) {
+        return new Response("Not found", { status: 404 });
+    }
+    return Response.json(todos[0]);
+};
+
+const getTodos = async () => {
+    const todos = await sql`SELECT * FROM todos`;
+    return Response.json(todos);
+
+};
+
+const addTodo = async (request) => {
+    let item;
+
+    try {
+        const jsonData = await request.json();
+        item = jsonData.item.trim();
+    } catch (e) {
+        return new Response("Bad request", { status: 400 });
+    }
+
+    if (!item || item.length === 0) {
+        return new Response("Bad request", { status: 400 });
+    }
+
+    await sql`INSERT INTO todos (item) VALUES (${ item })`;
+    return new Response("OK", { status: 200 });
+
+};
+
+const urlMapping = [
+    {
+        method: "GET",
+        pattern: new URLPattern({ pathname: "/todos/:id" }),
+        fn: getTodo,
+    },
+    {
+        method: "GET",
+        pattern: new URLPattern({ pathname: "/todos" }),
+        fn: getTodos,
+    },
+    {
+        method: "POST",
+        pattern: new URLPattern({ pathname: "/todos" }),
+        fn: addTodo,
+    },
+    {
+        method: "GET",
+        pattern: new URLPattern({ pathname: "/" }),
+        fn: handleGetRoot,
+    },
+];
+
+const handleRequest = async (request) => {
+    const mapping = urlMapping.find(
+        (um) => um.method === request.method && um.pattern.test(request.url)
+    );
+
+    if (!mapping) {
+        return new Response("Not found", { status: 404 });
+    }
+
+    const mappingResult = mapping.pattern.exec(request.url);
+    try {
+        return await mapping.fn(request, mappingResult);
+    } catch (e) {
+        console.log(e);
+        return new Response(e.stack, { status: 500 })
+    }
+};
+
+const portConfig = { port: 7777, hostname: '0.0.0.0' };
+Deno.serve(portConfig, handleRequest);
